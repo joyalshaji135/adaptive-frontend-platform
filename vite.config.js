10202d0166d54f67b9456f87afee37d2e9a653c6
joyalshaji135 <joyalshaji135@gmail.com>
2025-11-28 01:01:36 +0530

    feat(production): completed

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://adaptive-backend-platform.onrender.com',
         changeOrigin: true,
         secure: false,
       }
